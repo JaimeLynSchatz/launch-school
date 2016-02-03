982d3e2d380951f9d9b2d58e2d539efa27041006
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2016-02-02 22:16:54 -0800

    working through Hashes

---
+++ given.rb
@@ -0,0 +1,7 @@
+family = { uncles: ["bob", "joe", "steve"],
+           sisters: ["jane", "jill", "beth"],
+           brothers: ["frank", "rob", "david"],
+           aunts: ["mary", "sally", "susan"]
+         }
+
+p family.select{ |k| k == :sisters || k == :brothers }.values.flatten
