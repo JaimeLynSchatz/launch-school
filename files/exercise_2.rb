f0052594298e879d01a0b2003f2b6a07c227a3cc
JaimeLynSchatz <jaimelynschatz@gmail.com>
2016-02-07 22:16:54 -0800

    using Pathname

--- files/exercise_2.rb
+++ files/exercise_2.rb
@@ -1,4 +1,10 @@
+require 'pathname'
 d = Dir.new(".")
 while file = d.read do
   puts "#{file} has extension .txt" if File.extname(file) == ".txt"
 end
+
+pn = Pathname.new(".")
+pn.entries.each { |f| puts "#{f} has extension .txt" if f.extname == ".txt" }
+
+
