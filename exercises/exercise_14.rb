3bd66ea5d114130dbe3b13797a2d94f882ffa7c3
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2016-02-08 00:13:00 -0800

    compl exercise 14

--- exercises/exercise_14.rb
+++ exercises/exercise_14.rb
@@ -3,4 +3,10 @@ contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
                  ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
 
 contacts = {"Joe Smith" => {}, "Sally Johnson" => {}} 
+keys = contacts.keys
 
+contact_data.each_with_index do |contact, index|
+  contacts[keys[index]] = {email: contact[0], address: contact[1], phone: contact[2]}
+end
+
+puts contacts
